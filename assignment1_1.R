c77dd64d73c5ee0ebf27821f428bb03715b5e301
munmun55 <44106801+munmun55@users.noreply.github.com>
2018-12-27 23:23:13 +0530

    Add files via upload

---
+++ assignment1_1.R
@@ -0,0 +1,15 @@
+# Data-Analytics-with-R-Excel-Tableau_Session1 Assignment1
+#2.Recycling of elements in a vector 
+#3.Example of Recycling of elements in a vector
+
+a <- c(10,2,23,4)+c(2,10)
+print(a)
+
+b<- c(1,2,3,4,5,6,7) + c(1,3)
+print(b)
+
+x <- c(1,2,3,4,5,6)+c(2,10)
+print(x)
+
+y<- c(1,2,3,4,5,6,7) + c(10,30)
+print(y)
